0cdbd85cfa7019bec56d2605436f8a18608eb58e
chinguyen181120 <163628933+chinguyen181120@users.noreply.github.com>
2024-03-16 14:46:18 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,7 @@ public class Main {
         public SumThread(int[] array) {
             this.array = array;
         }
-
+ 
         @Override
         public void run() {
             int sum = 0;
